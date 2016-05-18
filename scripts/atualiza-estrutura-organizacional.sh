a1f04ee6dc4158f3b5d4e0aefddcd755f6bbabab
clayton@xdevel.com.br <clayton@xdevel.com.br>
2016-05-18 15:56:56 -0300

    inserido a estrutura organizacional do governo do espirito santo

--- scripts/atualiza-estrutura-organizacional.sh
+++ scripts/atualiza-estrutura-organizacional.sh
@@ -5,7 +5,7 @@ git pull --rebase
 
 
 echo 'Iniciando download lista de órgãos do SIORG'
-curl -sH 'Accept-encoding: gzip' "http://estruturaorganizacional.dados.gov.br/doc/estrutura-organizacional/resumida.json?retornarOrgaoEntidadeVinculados;=SIM" > src/main/resources/estrutura-organizacional.json.gz
+#curl -sH 'Accept-encoding: gzip' "http://estruturaorganizacional.dados.gov.br/doc/estrutura-organizacional/resumida.json?retornarOrgaoEntidadeVinculados;=SIM" > src/main/resources/estrutura-organizacional.json.gz
 echo 'Lista de órgãos do SIORG foi atualizada!'
 
 
